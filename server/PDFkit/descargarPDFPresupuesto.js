847516d0ce6673a7c33ecce2fca9c3d8e0934716
Dario Arteaga <daartv@gmail.com>
2017-07-04 17:41:42 -0400

    El servidor crea el presupuesto en PDF dinamicamente sin problemas

--- server/PDFkit/descargarPDFPresupuesto.js
+++ server/PDFkit/descargarPDFPresupuesto.js
@@ -3,5 +3,13 @@ const path = require('path');
 
 exports.descargarPDFPresupuesto = (req, res) => {
   console.log('funcion invocada');
-  res.download(__dirname + '/presupuesto.pdf', 'presupuesto.pdf');
+  res.download(__dirname + '/presupuesto.pdf', 'presupuesto.pdf', () => {
+    fs.unlink(__dirname + '/presupuesto.pdf', (error) => {
+      if (error) {
+        console.log('there was an error deleting', error);
+      } else {
+        console.log('Succesfully deleted');
+      };
+    });
+  });
 }
--- server/PDFkit/hacerPDFPresupuesto.js
+++ server/PDFkit/hacerPDFPresupuesto.js
@@ -3,30 +3,69 @@ const fs = require('fs');
 const path = require('path');
 
 exports.hacerPDFPresupuesto = (req, res) => {
-  let myDoc = new pdf;
-  myDoc.pipe(fs.createWriteStream(`PDFkit/presupuesto.pdf`));
-  myDoc.font('Times-Roman')
-       .fontSize(12)
-       .text(`${req.body.nombre} ${req.body.telefono} ${req.body.email} ${req.body.carrito} ${req.body.subTotal} ${req.body.total} ${req.body.IVA}`);
-  myDoc.end();
+  console.log('este es el carrito', req.body.carrito);
+  let { 
+    carrito,
+    subTotal,
+    total,
+    IVA
+  } = req.body;
+  let doc = new pdf;
+  doc.pipe(fs.createWriteStream(`PDFkit/presupuesto.pdf`));
+  doc.font('Times-Roman')
+        .fontSize(24)
+        .text('Royal Plast C.A.', {align: 'center'})
 
-/*  let file = path.join(__dirname, `${req.body.nombre}-presupuesto.pdf`);
-  res.download(file, function (err) {
-      if (err) {
-          console.log("Error");
-          console.log(err);
-      } else {
-          console.log("Success");
+        .fontSize(20)
+        .text('Presupuesto', {align: 'center'})
+           
+        .text('______________________________________________', {stroke: true})
+      
+      .moveDown()
+      .fontSize(16)
+      .text('Cantidad', {align: 'left', underline: 'true'})
+
+      .moveUp()
+      .text('Producto', {align: 'center', underline: 'true'})
+
+      .moveUp()
+      .text('Precio', {align: 'right', underline: 'true'})
+
+      for (let producto of carrito) {
+        doc.moveDown()
+          .fontSize(16)
+          .text(`${producto.cantidad}`, {align: 'left'})
+          .moveUp()
+          .text(`${producto.nombre}`, {align: 'center'})
+          .moveUp()
+          .text(`${producto.precio}`, {align: 'right'})
       }
-  });*/
 
-  let stat = fs.statSync(`/Users/dario/Desktop/Projects/corpo-royal/server/PDFkit/presupuesto.pdf`);
-  res.writeHead(200, {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/pdf',
-    'Content-disposition': `attachment;filename=[presupuesto].pdf`,
-    'Content-Length': stat.size
-  });
-  res.send();
+      doc.text('__________________________________________________________')
+        .text(`Subtotal: ${subTotal} BsF`, {align: 'right'})
+        .moveDown()
+        .text(`IVA: ${IVA} BsF`, {align: 'right'})
+        .moveDown()
+        .text(`Total: ${total} BsF`, {align: 'right'})
+
+        .moveUp()
+        .text('__________________________________________________________')
+        .fontSize(12)
+        .text('Royal Plast CA - RIF: J-30924274-1', {align: 'right'})
+        .text('Carrera 1 esq. Calle D Galpón Nro. 2-30A Zona Industrial III.Comdibar', {align: 'right'})
+        .text('Barquisimeto, Estado Lara', {align: 'right'})
+        .text('+58 251 2690061', {align: 'right'})
+        .text('+58 251 2692024', {align: 'right'})
+        .text('royalplastventas@gmail.com', {align: 'right'})
+
+
+
+
+  doc.end();
+
+  res.setHeader('access-control-allow-origin', '*');
+  res.status(200);
+  res.end();
 }
 
+// .text(`${req.body.nombre} ${req.body.telefono} ${req.body.email} ${req.body.carrito} ${req.body.subTotal} ${req.body.total} ${req.body.IVA}`);
